de611c6cd1477fac41b2ebe78a0da4113c74179f
Utkarsh <btoshine774@gmail.com>
2023-10-24 12:38:00 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+module.exports.handler = async(event) => {
+    return{
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({message:'Hello from Youtube Demo api'})
+    }};
